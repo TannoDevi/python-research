64b680223122f8af024ebc1cdda46a15530d277f
Tanno Devi <tannodevi84@gmail.com>
2024-04-18 10:15:25 +1200

    comparison.py

---
+++ comparison.py
@@ -0,0 +1,14 @@
+apple = 0 
+orange = 5
+fruit_basket = apple + orange
+fruit_juice = apple // orange
+print(f"I have {fruit_basket} fruits in my basket. That is enough for {fruit_juice} rounds of fruit juice")
+
+#comparison operation
+cookies_in_jar = 20
+chocolates_in_box = 15
+
+if cookies_in_jar > chocolates_in_box:
+    print("The cookies jar is winning! It is just a sweet victory!")
+else:
+    print("The chocolates are putting up a good fight, but the cookies right crumble")
