1ed4a4fecbe900523d08e975b636fb90068370a7
Tanno Devi <tannodevi84@gmail.com>
2024-04-17 23:19:07 +1200

    List.py

---
+++ List.py
@@ -0,0 +1,17 @@
+current_mood = "happy"
+previous_mood = "grumpy"
+# upgrade your mood
+current_mood += "and Excited"
+grocery_bag = ["apple", "kiwi","ice_cream", ""]
+print("Today, i am feeling{current_mood}. that's is a major mood upgrade")
+mode = input("Are you feeling happy now?").lower()
+if current_mood != "happy":
+    snack = input("Any snack you liker")
+
+#membership operator
+    
+if "chocolate" in grocery_bag:
+    print("Found chocolate in not your bag. pick another bag")
+else:
+    print("There is no chocolate. I hope you are feeling betterwith another snak anyway")
+    print("I hope you have a good day")
